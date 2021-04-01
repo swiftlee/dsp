4126dc2994d979f9e4c311f014e164f9877371d3
Blake Williams <michaelbwilliams100@gmail.com>
2021-04-01 16:17:09 -0400

    Clean up script a bit/debugging click noise + readme update again

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
   - On Windows, run: `venv\Scripts\activate.bat` <br/>
 On Unix or MacOS, run: `source venv/bin/activate`
 - To install dependencies, run `python -m pip install -r requirements.txt`
+- To setup the correct linter, install the python extension in vscode and set the format provider to `black`
 - Run `py main.py` (on MacOS/Linux, run `python main.py`). Profit.
--- main.py
+++ main.py
@@ -4,44 +4,63 @@ import matplotlib.pyplot as plt
 import numpy as np
 import utility
 
-azimuths = np.array([-80, -65, -55, *range(-45, 50, 5), 55, 65, 80])
-
-# elevations = -45 + 5.625 * range(0, 49)
-elevations = -45
-for x in range(0, 49):
-    elevations += 5.625 * x
-# print(azimuths)
-# print(elevations)
-aIndex = 0
-eIndex = 9
-
-np.set_printoptions(precision=6)
-
-samplerate, data = wavfile.read("thistle.wav")
-print(samplerate)
-hrtf = loadmat("CIPIC_58_HRTF.mat")
-hrir_r = hrtf["hrir_r"]
-hrir_l = hrtf["hrir_l"]
-
-# for i in range(1, len(hrir_l)):
-#     print(f"Index: {i} entry: {hrir_l[i]}")
-
-ITD = hrtf["ITD"]
-
-left_channel = data[:, 0]
-right_channel = data[:, 1]
+# declare variables
 result_left = []
 result_right = []
 increment = 1
 current_sample_index = 0
+aIndex = 0
+eIndex = 9
+azimuths = np.array([-80, -65, -55, *range(-45, 50, 5), 55, 65, 80])
+elevations = -45
+for x in range(0, 49):  # elevations = -45 + 5.625 * range(0, 49)
+    elevations += 5.625 * x
+
+np.set_printoptions(precision=6)
+
+"""Read in the song file, 'data' will be a 2D array with one column for each channel"""
+samplerate, data = wavfile.read("thistle.wav")
+# print(data)
+# data = utility.pcm2float(np.asarray(data).astype("int16"), "float32")
+# print(data)
+# rows = len(data)
+# columns = len(data[0])
+# print(f"rows: {rows} columns: {columns}")
+
+"""Set the left/right channel and frame size variables"""
+left_channel = data[:, 0]  # get all rows in column 0 (left channel)
+right_channel = data[:, 1]  # get all rows in column 1 (right channel)
 frame_size = int(np.ceil(samplerate / 2))
-print(frame_size)
+
+"""Load in and initialize the relevant variables from the HRTF"""
+hrtf = loadmat("CIPIC_58_HRTF.mat")
+hrir_r = hrtf["hrir_r"]
+hrir_l = hrtf["hrir_l"]
+ITD = hrtf["ITD"]  # 2D array with many columns/rows
+
+# more debugging
+# s = (2400, 2)
+# buffer = np.zeros(s)
+
+"""Iterate until our current index exceeds the data length"""
 while current_sample_index < len(data):
+    # debugging stuff
+    # current_sample_index = int(np.ceil(len(data) / 2))
+    # frame_size = 100
+    # left_channel = left_channel[current_sample_index:]
+
+    """If on the next iteration current_sample_index will exceed len(data)
+    we must adjust the frame size to ensure we convolve the end of the song file"""
     if (current_sample_index + frame_size) > len(data):
         frame_size = len(data) - current_sample_index
+
+    """Clear wav_left and wav_right arrays"""
     wav_left = []
     wav_right = []
+
+    """Set the azimuth index"""
     aIndex = aIndex + increment
+
     if aIndex == 0:
         eIndex = 8
 
@@ -61,9 +80,12 @@ while current_sample_index < len(data):
 
     wav_left = left_channel[:frame_size]
     wav_right = right_channel[:frame_size]
-    # print(f"Wav_left length: {len(wav_left)}")
+
+    # np.savetxt("before_conv.txt", wav_left)
+
     wav_left = np.asarray(signal.convolve(wav_left, lft, mode="same"))
-    print(f"wav_left {len(wav_left)}")
+    # np.savetxt("after_conv.txt", wav_left)
+    # break
     wav_right = np.asarray(signal.convolve(wav_right, rgt, mode="same"))
 
     if aIndex < 12:
@@ -73,29 +95,21 @@ while current_sample_index < len(data):
         wav_left = np.append(np.zeros(abs(delay)), wav_left)
         wav_right = np.append(wav_right, np.zeros(abs(delay)))
 
+    # debug, remove later
+    # result_left = np.append(result_left, buffer)
+    # result_right = np.append(result_right, buffer)
+    # print(f"result_left: {result_left}")
+    # print(f"wav_left: {wav_left}")
     result_left = np.append(result_left, wav_left)
     # print(f"result length: {len(result_left)}")
 
     result_right = np.append(result_right, wav_right)
 
-    left_channel = left_channel[(frame_size + 10) :]
+    left_channel = left_channel[frame_size:]
     # print(f"left_channel length: {len(left_channel)}")
 
-    right_channel = right_channel[(frame_size + 10) :]
+    right_channel = right_channel[frame_size:]
     current_sample_index += frame_size
-
-# a[start:stop]  # items start through stop-1
-# a[start:]      # items start through the rest of the array
-# a[:stop]       # items from the beginning through stop-1
-# a[:]           # a copy of the whole array
-
-# if (aIndex < 13) %sound is on left so delay right
-#     wav_left = [wav_left zeros(size(1:abs(delay)))];
-#     wav_right =[zeros(size(1:abs(delay))) wav_right];
-# else
-#     wav_left = [zeros(size(1:abs(delay))) wav_left];
-#     wav_right =[wav_right zeros(size(1:abs(delay)))];
-# end
 # END OF WHILE LOOP
 
 
@@ -108,27 +122,14 @@ print(f"frame_size: {frame_size}")
 convolved_data = [0, 0]
 convolved_data[0] = result_left
 convolved_data[1] = result_right
-# print(f"length of {len(np.asarray(convolved_data).T)} {np.asarray(convolved_data)}")
-# print(f"but data length is: {len(np.asarray(data))}")
-# print(lft)
-# print("---------------------------")
-# print(rgt)
-# delay = hrtfStruct.ITD(aIndex, eIndex);
-# wav_left = [wav_left conv(lft, sig(:,1)')];
-# wav_right = [wav_right conv(rgt, sig(:,2)')];
 
-# np.asarray(convolved_data).astype("int16")
 
 # Before further processing, we normally want to convert the signals to floating point values
 # and normalize them to a range from -1 to 1 by dividing all values by the largest possible value.
 normalized = utility.pcm2float(np.asarray(convolved_data).T.astype("int16"), "float32")
-# print(normalized)
 soundToPlay = np.array([normalized[:, 0], normalized[:, 1]], dtype="float32")
 
 wavfile.write("scipy_float32.wav", samplerate, soundToPlay.T)
 wavfile.write("scipy_pcm16.wav", samplerate, utility.float2pcm(soundToPlay.T, "int16"))
 
-# https://docs.scipy.org/doc/scipy/reference/signal.html
-# np.convolve ?
-# scipy.signal.convolve ?
-# lft = np.squeeze()
+# # https://docs.scipy.org/doc/scipy/reference/signal.html
