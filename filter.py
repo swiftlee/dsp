792e2979d7a07661367fdb3fb138b73588f0b5f8
Jonathan Conlin <jconlin@ufl.edu>
2021-04-23 21:28:15 -0400

    added panning
    Co-authored-by: Blake Williams <michaelbwilliams100@gmail.com>

--- filter.py
+++ filter.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.signal import freqz
 import numpy as np
 import utility
+from pydub import AudioSegment
 
 thistle = Audio("thistle.wav")
 
@@ -18,3 +19,9 @@ filtered_result[1] = filtered_right
 normalized = utility.pcm2float(np.asarray(filtered_result).T.astype("int16"), "float32")
 soundToPlay = np.array([normalized[:, 0], normalized[:, 1]], dtype="float32")
 thistle.write_result('thistle-filtered.wav', utility.float2pcm(soundToPlay.T))
+
+filtered_thistle = AudioSegment.from_wav("thistle-filtered.wav")
+filtered_thistle = filtered_thistle.pan(-1)
+filtered_thistle.export("thistle-panned.wav", format="wav")
+
+
